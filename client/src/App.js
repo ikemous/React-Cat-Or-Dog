a3f36c775d4a9b91fe40aa58dfd8c5f6a5aa3766
ike barranco <barranco.ike@outlook.com>
2020-08-23 08:33:23 -0600

    added navbar alterations and container to welcom page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import TotallyAwesomeNavbar from "./components/TotallyAweomeNavbar.js";
+import WelcomePage from "./pages/WelcomePage.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App()
 {
   return (
     <>
-      
+      <TotallyAwesomeNavbar />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={WelcomePage} />
+        </Switch>
+      </Router>
     </>
   )
 }
---
+++ client/src/pages/WelcomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function WelcomePage()
+{
+    return (
+        <>
+            <Container fluid>
+
+            </Container>
+        </>)
+    ;
+}
+
+export default WelcomePage;
