0875c556f2eb4eb0e9f554aac8815aad58e87da8
Ike <barranco.ike@gmail.com>
2020-08-30 02:58:05 -0600

    commiting code to pull from laptop

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
-import { Card } from "react-bootstrap";
+import { Card, Row, Col, Button } from "react-bootstrap";
 import logo from "../logo.svg"
 import LikeOrDislikeForm from "./LikeOrDislikeForm.js"
 
-function ProfileCard({ animal, information, setUserAction })
+function ProfileCard({ animal, handleClick })
 {
     useEffect(() => console.log(animal), [animal]);
     return (
@@ -18,12 +18,19 @@ function ProfileCard({ animal, information, setUserAction })
                 </Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">{animal.gender|| "Unknown"} - {animal.age || "Unknown"}</Card.Subtitle>
                 <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
+                    {animal.bio || "Loading Bio"}
                 </Card.Text>
             </Card.Body>
             <Card.Body>
-                <LikeOrDislikeForm />
+            <Row>
+                <Col>
+                    <Button name="left" onClick={handleClick}>Left</Button>
+                </Col>
+                <Col></Col>
+                <Col>
+                    <Button name="right" onClick={handleClick}>Right</Button>
+                </Col>
+            </Row>
             </Card.Body>
         </Card>
     )
--- client/src/pages/SwipingPage.js
+++ client/src/pages/SwipingPage.js
@@ -6,40 +6,26 @@ import ProfileCard from "../components/ProfileCard.js";
 function SwipingPage()
 {
     const [animal, setAnimal] = useState({});
-    const [information, setInformation] = useState({});
-    // const [informatinSeed, setInformationSeed] = useState({});
     const [userAction, setUserAction] = useState(false)
     
     useEffect(() => {
         API.randomProfile()
         .then(({data: profileInfo}) => setAnimal(profileInfo))
         .catch(error => console.log(error));
-        // const randomNumber = Math.floor(Math.random() * 2);
-        // API.randomInformation()
-        // .then(async ({data: randomInfo}) => {
-        //     await setInformation(randomInfo.results[0]);
-        //     await setInformationSeed(randomInfo.info.seed);
-        //     if(randomNumber === 0)
-        //     {
-        //         API.randomCat()
-        //         .then(({ data:cat }) => setAnimal(cat))
-        //         .catch(error => console.log(error));    
-        //     }
-        //     else
-        //     {
-        //         API.randomDog()
-        //         .then(({data:dog}) => setAnimal(dog))
-        //         .catch(error => console.log(error));
-        //     }
-        // })
-        // .catch(error => console.log(error));
     }, [userAction]);
-    useEffect(() => console.log(information),[information]);
+
+    useEffect(() => console.log(animal),[animal]);
+
+    const handleClick = ({target}) => {
+        console.log(target.name);
+        if( target.name === "left") return setUserAction(true);
+    };
+
     return (
         <>
             <Container>
                 <Row>
-                    <ProfileCard animal={animal} information={information} setUserAction={setUserAction} />
+                    <ProfileCard animal={animal} handleClick={handleClick} />
                 </Row>
             </Container>
         </>
