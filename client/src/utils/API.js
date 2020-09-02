2a0ed5941f017d1ced9a2c4d54c3ef7700065663
Ike <barranco.ike@gmail.com>
2020-09-02 03:16:19 -0600

    added api calls

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,17 +4,8 @@ export default {
     createUser(userInfo) {
         return axios.post("/user/signup", userInfo);
     },
-    randomCat()
-    {
-        return axios.get("/cats/random");
-    },
-    randomDog() 
-    {
-        return axios.get("/dogs/random");
-    },
-    randomInformation()
-    {
-        return axios.get("/animal/information");
+    loginUser(userInfo) {
+        return axios.post("/user/login", userInfo);
     },
     randomProfile() {
         return axios.get("/profile");
