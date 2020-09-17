ab423f96a4491c67cdaa087a45ec1ff491cd95d4
Ike <barranco.ike@gmail.com>
2020-09-17 06:01:01 -0600

    added footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { FaFacebookSquare, FaGithubSquare, FaTwitterSquare, FaGlobe } from "react-icons/fa";
+
+function TotallyDecentFooter() {
+    return (
+        <footer className="page-footer pt-4">       
+            <div class="container-fluid text-center text-md-left">
+                <div class="row">
+                    <hr class="clearfix w-100 d-md-none pb-3" />
+                    <div className="col-md-3 mb-md-0 mb-3">
+
+                    <h5 className="text-uppercase">API's</h5>
+
+                    <ul className="list-unstyled">
+                        <li>
+                            <a href="https://dog.ceo/dog-api/" rel="noopener noreferrer" target="_blank">Dog Pictures API</a>
+                        </li>
+                        <li>
+                            <a href="https://thecatapi.com/" rel="noopener noreferrer" target="_blank">Cat Pictures API</a>
+                        </li>
+                        <li>
+                            <a href="https://randomuser.me" rel="noopener noreferrer" target="_blank">Random Information API</a>
+                        </li>
+                        <li>
+                            <a href="https://icanhazdadjoke.com" rel="noopener noreferrer" target="_blank">Dad Joke API</a>
+                        </li>
+                    </ul>
+
+                </div>
+
+                    <div class="col-md-3 mb-md-0 mb-3">
+
+                    <h5 class="text-uppercase">Creator Links</h5>
+
+                    <ul class="list-unstyled">
+                    <li>
+                        <FaFacebookSquare /> <a href="fb.gg/ikemous">/Ikemous</a>
+                    </li>
+                    <li>
+                        <FaGithubSquare /> <a href="https://github.com/ikemous" rel="noopener noreferrer" target="_blank">/ikemous</a>
+                    </li>
+                    <li>
+                        <FaTwitterSquare /> <a href="https://twitter.com/real_ikemous" rel="noopener noreferrer" target="_blank">/real_ikemous</a>
+                    </li>
+                    <li>
+                        <FaGlobe /> <a href="http://ikemous.com" rel="noopener noreferrer" target="_blank">Portfolio</a>
+                    </li>
+                    </ul>
+
+                </div>
+
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default TotallyDecentFooter;
--- client/src/pages/WelcomePage.js
+++ client/src/pages/WelcomePage.js
@@ -1,69 +1,71 @@
-import React, { useState } from "react";
+import React from "react";
+import TotallyDecentFooter from "../components/Footer.js"
 import layingDawg from "../components/images/picture1.jpg";
 import toungeDawg from "../components/images/picture7.jpg";
 import yellowDawg from "../components/images/picture4.jpg";
 import smallDawg from "../components/images/picture5.jpg";
 import catsCATS from "../components/images/picture6.jpg";
-import { Container, Carousel, Row } from "react-bootstrap";
+import { Container, Carousel, Row, Alert } from "react-bootstrap";
 
 function WelcomePage()
 {
-    const [index, setIndex] = useState(0);
-
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex);
-    };
 
     return (
-        <Container className="slideShow" style={{height: "calc(100vh - 56px)"}} fluid>
-        <Row style={{height: "inherit"}} className="align-items-center justify-content-center">
-            <div>
-                <h2 className="text-center">Find your newest furry friend!</h2>
-                <Carousel style={{height: "80vh", margin: "20px"}} activeIndex={index} onSelect={handleSelect}>
-                    <Carousel.Item>
-                        <img
-                            style={{height: "-webkit-fill-available"}}
-                            className="d-block w-100 slideImage"
-                            src={layingDawg}
-                            alt="First slide"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            style={{height: "-webkit-fill-available"}}
-                            className="d-block w-100 slideImage"
-                            src={toungeDawg}
-                            alt="Second slide"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            style={{height: "-webkit-fill-available"}}
-                            className="d-block w-100 slideImage"
-                            src={yellowDawg}
-                            alt="Third slide"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            style={{height: "-webkit-fill-available"}}
-                            className="d-block w-100 slideImage"
-                            src={catsCATS}
-                            alt="Third slide"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            style={{height: "-webkit-fill-available"}}
-                            className="d-block w-100 slideImage"
-                            src={smallDawg}
-                            alt="Third slide"
-                        />
-                    </Carousel.Item>
-                </Carousel>
-            </div>
-        </Row>
-
+        <Container style={{height: "calc(100vh - 56px)"}}>
+            <Row style={{height: "80vh"}} className="align-items-center justify-content-center">
+                <div>
+                    <Alert style={{margin: "20px", textAlign: "center"}} variant="danger">
+                        <Alert.Heading>This Is For Fun!</Alert.Heading>
+                        <p>
+                            This Site Is Pure For Fun! The Animals/Names/Infos are fictional.
+                        </p>
+                    </Alert>
+                    <Carousel className="slideShow" style={{height: "45vh", margin: "20px"}}>
+                        <Carousel.Item>
+                            <img
+                                style={{height: "45vh"}}
+                                className="d-block w-100 slideImage"
+                                src={layingDawg}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                style={{height: "45vh"}}
+                                className="d-block w-100 slideImage"
+                                src={toungeDawg}
+                                alt="Second slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                style={{height: "45vh"}}
+                                className="d-block w-100 slideImage"
+                                src={yellowDawg}
+                                alt="Third slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                style={{height: "45vh"}}
+                                className="d-block w-100 slideImage"
+                                src={catsCATS}
+                                alt="Third slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                style={{height: "45vh"}}
+                                className="d-block w-100 slideImage"
+                                src={smallDawg}
+                                alt="Third slide"
+                            />
+                        </Carousel.Item>
+                    </Carousel>
+                    <h2 className="text-center">Find your newest furry friend!</h2>
+                </div>    
+            </Row>
+            <TotallyDecentFooter />
         </Container>
         )
     ;
